6efd0c116a34b1fb1863e1dde991679b1d910d7f
SarangTandel5112 <sarangtandel5112@gmail.com>
2023-02-20 21:56:36 +0530

    basic setup

---
+++ src/Components/TopBarComponent.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import styles from "./TopBarComponent.module.css";
+
+const TopBarComponent: FunctionComponent = () => {
+    return (
+        <div>
+            trial
+        </div>
+
+    );
+};
+
+export default TopBarComponent;
